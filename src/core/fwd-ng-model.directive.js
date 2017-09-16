c185a2aa660fe5732c2d8c0adc8ef8ae3f020fa4
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-16 13:30:04 +0200

    Add fwdNgModel directive to use ngModel from <bs-input>

---
+++ src/core/fwd-ng-model.directive.js
@@ -0,0 +1,39 @@
+/**
+ * fwdNgModel
+ *
+ * Useful for passing an NgModelController instance through to
+ * a component (e.g. an input). This allows us to use all the usual
+ * directives that ngModel uses.
+ *
+ * General:
+ * - ngChange
+ * - ngModelOptions
+ *
+ * Validations:
+ * - ngMaxlength/ngMinlength
+ * - ngPattern
+ * - ngRequired
+ */
+const data = {
+    scope: {
+        fwdNgModel: '&',
+    },
+    link: {
+        pre(scope, element, attrs) {
+            let ngModelCtrl = scope.fwdNgModel();
+            if (!ngModelCtrl) {
+                let msg = `fwdNgModel: expected ngModel instance, got ${attrs.fwdNgModel}`;
+                throw Exception(msg);
+            }
+            element.data('$ngModelController', ngModelCtrl);
+        },
+    },
+};
+
+function directive() {
+    directive.name = 'fwdNgModel';
+    return data;
+}
+
+export default directive;
+
---
+++ src/core/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+import fwdNgModelDirective from './fwd-ng-model.directive';
+
+
+export default
+    angular.module('bootstrap.core', [])
+        .directive(fwdNgModelDirective.name, fwdNgModelDirective)
+        .name;
