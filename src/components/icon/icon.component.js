49625d4258dc8d5b5fb6cff06c071dd3c2f4fcda
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-16 21:59:20 +0200

    Add bsIcon to show Octicons

---
+++ src/components/icon/icon.component.js
@@ -0,0 +1,49 @@
+/**
+ * bsIcon
+ *
+ * Displays Octicons (https://github.com/primer/octicons) with configuration.
+ *
+ * Specify the icon with the 'symbol' property:
+ *   <bs-icon symbol="mark-github"></bs-icon>
+ *
+ * Additionally supports the following properties:
+ * - size (for equal width and height)
+ * - width, height separately
+ * - aria-label
+ * - class
+ *
+ * Setting class="light" will invert the color
+ */
+import octicons from 'octicons';
+
+import './icon.component.css';
+
+const PROPS = [
+    'width',
+    'height',
+    'aria-label',
+    'class',
+];
+
+export default {
+    name: 'bsIcon',
+    template($attrs) {
+        if (!octicons.hasOwnProperty($attrs.symbol)) {
+            throw Exception(`bsIcon: no icon with symbol '${this.symbol}'`);
+        }
+
+        // Properties
+        let config = {};
+        for (let p of PROPS) {
+            if (!$attrs.hasOwnProperty(p)) {
+                continue;
+            }
+            config[p] = $attrs[p];
+        }
+        if ($attrs.hasOwnProperty('size')) {
+            config['width'] = $attrs.size;
+            config['height'] = $attrs.size;
+        }
+        return octicons[$attrs.symbol].toSVG(config);
+    },
+};
--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,12 @@ import angular from 'angular';
 
 import core from '../core';
 import Input from './input/input.component';
+import Icon from './icon/icon.component';
+
 
 
 export default
     angular.module('bootstrap.components', [core])
         .component(Input.name, Input)
+        .component(Icon.name, Icon)
         .name;
