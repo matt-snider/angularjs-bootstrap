8ef7e9a914e5204929bfc6f0ce083ab764929342
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-21 22:10:53 +0200

    Use createReplaceDirective() for bsLabel

--- src/core/index.js
+++ src/core/index.js
@@ -1,12 +1,19 @@
 import angular from 'angular';
+import {createReplaceDirective} from './utils';
 
-import cssInjector from './css-injector.provider';
 import fwdNgModel from './fwd-ng-model.directive';
 import transcludeAndReplace from './transclude-and-replace.directive';
+
+import cssInjector from './css-injector.provider';
+
+
+// Generated
+let label = createReplaceDirective("bsLabel");
 
 export default
     angular.module('bootstrap.core', [])
         .directive(fwdNgModel.name, fwdNgModel)
+        .directive(label.name, label)
         .directive(transcludeAndReplace.name, transcludeAndReplace)
         .provider(cssInjector.name, cssInjector)
         .name;
