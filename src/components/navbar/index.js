32c9806360ae6fdd9b36871731689b146cb403eb
Matt Snider <matt.snider@alum.utoronto.ca>
2017-09-21 22:18:32 +0200

    bsNavbar: use createReplaceDirective() for brand

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,9 +4,18 @@ import Navbar from './navbar.component';
 import Items from './items.directive';
 import Item from './item.directive';
 
+
+// Generated
+let NavbarBrand = createReplaceDirective(
+    'bsNavbarBrand',
+    '<a href="#" class="navbar-brand"></a>',
+    true,
+)
+
 export default
     angular.module('bootstrap.navbar', [])
         .component(Navbar.name, Navbar)
+        .directive(NavbarBrand.name, NavbarBrand)
         .directive(Items.name, Items)
         .directive(Item.name, Item)
         .name;
