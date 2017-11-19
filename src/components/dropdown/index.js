f6d03d5c30af4a2f90abdbe4050e951639d646c9
Matt Snider <matt.snider@alum.utoronto.ca>
2017-11-19 21:04:49 +0100

    bsDropdown: remove unused code and add bsDropdownDivider

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -5,17 +5,24 @@ import core from '../../core';
 import Dropdown from './dropdown.component';
 
 // Generated
-let DropdownItem = createReplaceDirective(
-    "bsDropdownItem",
+let dropdownItem = createReplaceDirective(
+    'bsDropdownItem',
     '<a href="#" class="dropdown-item"></a>',
     true,
 );
-let DropdownItems = createReplaceDirective("bsDropdownItems");
+
+let dropdownDivider = createReplaceDirective(
+    'bsDropdownDivider',
+    '<div class="dropdown-divider"></div>',
+);
+
+let dropdownItems = createReplaceDirective("bsDropdownItems");
 
 
 export default
     angular.module('bootstrap.dropdown', [core])
         .component(Dropdown.name, Dropdown)
-        .directive(DropdownItem.name, DropdownItem)
-        .directive(DropdownItems.name, DropdownItems)
+        .directive(dropdownItem.name, dropdownItem)
+        .directive(dropdownDivider.name, dropdownDivider)
+        .directive(dropdownItems.name, dropdownItems)
         .name;
