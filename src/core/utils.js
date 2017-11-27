fbcaa01e34a832c24854e249685d4d2e015a7ff6
Matt Snider <matt.snider@alum.utoronto.ca>
2017-11-28 00:08:51 +0100

    utils: refactor createReplaceDirective() and allow passing template fn for wrapWith

--- src/core/utils.js
+++ src/core/utils.js
@@ -20,40 +20,47 @@ import angular from 'angular';
  *      <bs-some-inner-component></bs-some-inner-component>
  *    </ul>
  *
- * @param {*} name
- * @param {*} wrapWith
+ * @param {*} name the name the directive should have (simply sets name property)
+ * @param {*} wrapWith a string to use as a wrapper around whatever content is transcluded
+ *            OR a template function that should return a wrapper when passed tElement & tAttrs.
  * @param {*} copyAttributes
  */
 export function createReplaceDirective(name, wrapWith, copyAttributes = false) {
 
-    function template(tElement, tAttrs) {
-        let elem = angular.element(wrapWith);
-        elem.append(tElement.html());
-        if (copyAttributes) {
-            for (let prop of Object.keys(tAttrs.$attr)) {
-                elem.attr(prop, tAttrs[prop]);
-            }
-        }
-        return elem[0].outerHTML;
-    }
-
-    // This is needed if we don't wrap the element since
-    // replace
-    function compile(tElement, tAttr) {
-        return ($scope, $element) => {
-            if ($element.children().length) {
-                $element.replaceWith($element.children());
-            } else {
-                $element.replaceWith($element.html());
-            }
-        };
-    }
-
+    // If wrapWith is not passed, we have a simple compile directive that
+    // replaces the $element with it's children or html contents.
+    // Otherwise, we need to dynamically create a template based on wrapWith,
+    // insert the $elements html contents, and then copy the attributes.
     let implementation;
-    if (wrapWith) {
-        implementation = { template };
+    if (!wrapWith) {
+        implementation = {
+            compile(tElement, tAttr) {
+                return ($scope, $element) => {
+                    if ($element.children().length) {
+                        $element.replaceWith($element.children());
+                    } else {
+                        $element.replaceWith($element.html());
+                    }
+                };
+            },
+        };
     } else {
-        implementation = { compile };
+        implementation = {
+            template(tElement, tAttrs) {
+                let wrapper = wrapWith;
+                if (typeof wrapWith === 'function') {
+                    wrapper = wrapWith(tElement, tAttrs);
+                }
+                let elem = angular.element(wrapper);
+                elem.append(tElement.html());
+                if (copyAttributes) {
+                    for (let prop of Object.keys(tAttrs.$attr)) {
+                        elem.attr(prop, tAttrs[prop]);
+                    }
+                }
+                return elem[0].outerHTML;
+            },
+        };
     }
 
     // Merge the implementation and additional props to
