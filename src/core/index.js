371af10f714edac69ed17845d7290953b227e123
Matt Snider <matt.snider@alum.utoronto.ca>
2018-03-02 21:32:28 +0100

    REmove bsLabel replacementDirective

--- src/core/index.js
+++ src/core/index.js
@@ -7,14 +7,9 @@ import backdrop from './backdrop.factory';
 import cssInjector from './css-injector.provider';
 import contextualTypes from './contextual-types.service';
 
-
-// Generated
-let label = createReplaceDirective("bsLabel");
-
 export default
     angular.module('bootstrap.core', [])
         .directive(fwdNgModel.name, fwdNgModel)
-        .directive(label.name, label)
         .factory(backdrop.name, backdrop)
         .provider(cssInjector.name, cssInjector)
         .service(contextualTypes.name, contextualTypes)
